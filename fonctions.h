5e468c062e2bcaef91f9ea6e44b2734b9429c8b2
arnaud355 <33963757+arnaud355@users.noreply.github.com>
2019-04-30 20:13:56 +0200

    Create fonctions.h

---
+++ fonctions.h
@@ -0,0 +1,6 @@
+void recupererMot(char* motADeviner);
+void redimensionneTaille(char** mot, int taille);
+void redimensionneTNumerique(int** tab, int taille);
+char lireCaractere();
+void viderBuffer();
+int lire(char *chaine, int longueur);
